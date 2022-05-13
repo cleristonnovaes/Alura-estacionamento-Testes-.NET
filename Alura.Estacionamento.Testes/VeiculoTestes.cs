dcee3aba614be62a67393b54b9d6baa93e63602d
cleristonnovaes <novaes.cleriston@gmail.com>
2022-05-13 10:26:33 -0300

    Utilizando Theory e InlineData

--- Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -1,3 +1,4 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
 using System;
 using Xunit;
@@ -27,6 +28,14 @@ namespace Alura.Estacionamento.Testes
             veiculo.Frear(10);
             //Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            var veiculo = new Veiculo();
+
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
         }
     }
 }
